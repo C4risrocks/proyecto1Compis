eafcea41467c9958384029a1480d9db25ae88e99
C4risrocks <c4risrocks@hotmail.com>
2018-04-01 12:13:20 -0500

    Archivos Ordenados

--- archivo/archivo.cpp
+++ archivo/archivo.cpp
@@ -7,37 +7,37 @@ int main(){
 	char c;
 	int countline=0;
 	int i=0;
-	char *palabra;
-	
+	char palabra[30];
+
 	if(apt==NULL){
 		perror("ERROR: 404");
 		return 0;
 	}else{
-		
-		
+
+
 		while (feof(apt) == NULL) {
-			
+
 			i=0;
-			for(int j=0;j<strlen(palabra);j++)
+			for(int j=0;j<30;j++)
 			palabra[j]=' ';
-			
-			
+
+
 			do{
 				c = fgetc(apt);
 				palabra[i]=c;
 				i++;
 			}while(c!=' '||c=='\n'||c== EOF||c=='\t');
-			
+
 			fprintf(nuevo, "%s",palabra);
-			
-			if (c=='\n'||c== EOF) 
+
+			if (c=='\n'||c== EOF)
 			countline++;
 			printf("%c\n",c);
-			
+
 		}
 		printf ("%i ",countline);
-		
-		
+
+
 	}
 
 }
